14ef13b08c97d9597cf73670ba365b2f6f58667f
Dim <skyuphigh01@gmail.com>
2018-05-20 10:32:33 -0400

    Temporary landing page

--- server/server.go
+++ server/server.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+const port = ":8002"
+
+func main() {
+	app := gin.New()
+
+	app.StaticFile("/", "./index.html")
+
+	app.Run(port)
 }
